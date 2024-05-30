fc27865e089a85239dbe976ac383ee5d4e607daf
Torham <torhamdev@gmail.com>
2024-05-30 08:30:51 -0700

    Add page not found and method support error

--- server.py
+++ server.py
@@ -1,25 +1,40 @@
 import socket
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 8001  # Port to listen on (non-privileged ports are > 1023)
+PORT = 8002  # Port to listen on (non-privileged ports are > 1023)
+
+
+def send_response(conn: socket.socket, data: bytes, status_code: int):
+    res = (
+        f"HTTP/1.1 {status_code}\r\nContent-Length: {len(data)}\r\n\r\n{data}".encode()
+    )
+    conn.send(res)
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print(f"Listing at {HOST}:{PORT}")
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    data = conn.recv(1024)
-    http_req = data.decode().split("\r\n")
-    req_method, path, http_version = http_req[0].split(" ")
 
-    print(req_method, path, http_req)
+    while True:
+        conn, addr = s.accept()
+        data = conn.recv(1024)
+        http_req = data.decode().split("\r\n")
+        req_method, path, http_version = http_req[0].split(" ")
 
-    to_serve = "index.html" if path == '/' else path[1:]
+        if http_version != "HTTP/1.1":
+            send_response(conn=conn, data="", status_code=505)
 
-    with open(f"./super_cool_website/{to_serve}") as f:
-        to_serve = f.read()
+        if req_method != "GET":
+            send_response(conn=conn, data="Sorry we only support GET.", status_code=405)
 
-    response = f"HTTP/1.1 200\r\nContent-Length: {len(to_serve)}\r\n\r\n{to_serve}".encode()
-    print(response)
-    conn.send(response)
-    conn.close()
+        to_serve = "index.html" if path == "/" else path[1:]
+
+        try:
+            with open(f"./super_cool_website/{to_serve}") as f:
+                to_serve = f.read()
+        except FileNotFoundError:
+            send_response(conn=conn, data="<h1>Page Not Found!</h1>", status_code=404)
+    
+        send_response(conn=conn, data=to_serve, status_code=200)
+        conn.close()
